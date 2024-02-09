d56c2a62e581d0411821fdf5ea3f7134efc251d6
JohnProphet <87950318+JohnProphet@users.noreply.github.com>
2024-02-08 20:19:00 -0600

    Passing 17 of 18 TestTaskTwo

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -2,14 +2,18 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.Optional;
 
+//@Autowired
+//private interface EmployerRepository {} employerRepository {}
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -12,9 +12,8 @@ import java.util.Objects;
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
-    @NotNull
-    @NotEmpty
     @GeneratedValue
+    @NotNull
     private int id;
 
     @NotNull
