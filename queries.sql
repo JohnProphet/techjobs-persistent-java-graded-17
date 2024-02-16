3f96bf06bb5d25028006461534de4c294ee7d481
JohnProphet <johnldudash@gmail.com>
2024-02-16 13:31:26 -0600

    On to Task3!

--- queries.sql
+++ queries.sql
@@ -1,6 +1,13 @@
 --Part 1
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_schema = 'techjobs'
+AND table_name = 'job';
 
 --Part 2
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 --Part 3
 
