53d318b5bcf2530ed59045e55334259e92c222ff
JohnProphet <johnldudash@gmail.com>
2024-02-17 08:00:06 -0600

    Task3 red box

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,20 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // Employer is a class that will be mapped to one of our tables. Make sure the class has the
 // @Entity annotation, as well as the no-arg constructor required for Hibernate to create an
 // object.
 
-// public class Employer {}
 @Entity
-@NotNull
 public class Employer extends AbstractEntity {
     @NotNull
     @Size(min = 3, max = 75)
     private String location;
+    @OneToMany(mappedBy = "jobs")
+    @JoinColumn(name="employer")
+    private List<Job> jobs = new ArrayList<>();
 
     public String getLocation() {
         return location;
@@ -23,9 +29,14 @@ public class Employer extends AbstractEntity {
     public void setLocation(String newLocation) {
         location = newLocation;
     }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> newJobs) {
+        jobs = newJobs;
+    }
+
     public Employer() {}
 }
-
-
-
-
